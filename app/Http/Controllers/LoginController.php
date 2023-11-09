1e78bf0c0d8ea69fb4d498f9d3221515c0688d6d
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-11-09 08:54:02 -0600

    werer

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,6 +11,10 @@ class LoginController extends Controller
     }
 
     public function handleLogin(Request $request){
-        dd($request->all());
+        $request->validate([
+            "name" => "required"
+        ]); 
+       
+        return $request;
     }
 }
